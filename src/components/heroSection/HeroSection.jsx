a979b21e287ca8eaaef3954fe49a944919d34d76
thenithin342 <thenithin342@gmail.com>
2025-04-10 16:40:21 +0530

    Update HeroSection.jsx

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -4,7 +4,7 @@ function HeroSection() {
   return (
     <div>
         <video width="100%" height="auto" loop muted autoPlay>
-            <source src="WhatsApp Video 2025-04-05 at 08.41.51_56f851df.mp4" type="video/mp4" />
+            <source src="https://videos.pexels.com/video-files/2268807/2268807-hd_1920_1080_24fps.mp4" />
             Your browser does not support the video tag.
         </video>
     </div>
